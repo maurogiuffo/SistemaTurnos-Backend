2cb0e3a0c7ecaa577325466be50df84739bbe66a
MauroGiuffo <maurogiuffo@gmail.com>
2020-05-13 21:39:06 -0300

    proyecto incial

---
+++ src/main/java/utn/metodologiasistemas2/sistematurnos/SistemaTurnosApplication.java
@@ -0,0 +1,13 @@
+package utn.metodologiasistemas2.sistematurnos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaTurnosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaTurnosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/utn/metodologiasistemas2/sistematurnos/SistemaTurnosApplicationTests.java
@@ -0,0 +1,13 @@
+package utn.metodologiasistemas2.sistematurnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaTurnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
